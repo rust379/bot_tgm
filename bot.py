caf58e3dadde02324393e2423dad5afafb127975
Светлана <asmelasmm@gmail.com>
2020-05-04 21:25:28 +0300

    Rename cf login to handle; Rename cf register command

--- bot.py
+++ bot.py
@@ -12,13 +12,21 @@ class BotRun(telebot.TeleBot):
     database: type(db.Database)
 
     def __init__(self, token):
-        """Init telegram bot with token"""
+        """Init telegram bot with token.
+
+        Args:
+            token: string with telegram bot token
+        """
         self.current_event = dict()
 
         super().__init__(token)
 
     def set_database(self, database):
-        """Set database"""
+        """Set database for further use
+
+        Args:
+            database: database.Database() class
+        """
         self.database = database
 
     def run(self):
@@ -38,6 +46,9 @@ def welcome(message):
                         message.from_user, BOT.get_me())
     BOT.send_message(message.chat.id, message_text, parse_mode="html")
 
+    BOT.database.insert_into_table(
+        "users", [message.from_user.username, None, message.chat.id])
+
 
 @BOT.message_handler(commands=["help"])
 def commands_list(message):
@@ -51,23 +62,23 @@ def commands_list(message):
                      parse_mode="html")
 
 
-@BOT.message_handler(commands=["register"])
-def register(message):
+@BOT.message_handler(commands=["registerCfHandle"])
+def register_cf_handle(message):
     """Change current action to registartion for chat id"""
     BOT.send_message(message.chat.id,
                      "Пожалуйста, скажите нам ваш логин на codeforces")
-    BOT.current_event[str(message.chat.id)] = "REGISTRATION"
+    BOT.current_event[str(message.chat.id)] = "CF_REGISTRATION"
 
 
 @BOT.message_handler(commands=["myRating"])
 def cur_user_rating(message):
-    """Send info about current rating, if chat id was registered with cf_login"""
+    """Send info about current rating, if chat id was registered with cf_handle"""
     params = db.get_request_struct()
-    params["attributes"].append("cf_login")
+    params["attributes"].append("cf_handle")
     params["conditions"].append("chat_id = {}".format(message.chat.id))
-    cf_login = BOT.database.data_from_table('users', params)
-    if cf_login:
-        message_text = cf.curUserRating(cf_login[0][0])
+    cf_handle = BOT.database.data_from_table('users', params)
+    if cf_handle:
+        message_text = cf.curUserRating(cf_handle[0][0])
         BOT.send_message(message.chat.id, message_text, parse_mode="html")
 
 
@@ -76,11 +87,14 @@ def text_message_handler(message):
     """Analyze user"s current event and performs an action based on it
 
     Possible values for event:
-        "REGISTRATION" - register user codeforces login in database
+        "CF_REGISTRATION" - register user codeforces login in database
     If nothing event provided - ignore message"""
+    if str(message.chat.id) not in BOT.current_event.keys():
+        return
 
-    if BOT.current_event[str(message.chat.id)] == "REGISTRATION":
-        BOT.database.insert_into_table(
-            "users",
-            [message.from_user.username, message.text, message.chat.id])
+    if BOT.current_event[str(message.chat.id)] == "CF_REGISTRATION":
+        BOT.database.update_record("users", [
+            'tgm_name = "{}" AND chat_id = {}'.format(
+                message.from_user.username, message.chat.id)
+        ], 'cf_handle = "{}"'.format(message.text))
         BOT.current_event.pop(message.chat.id, None)
--- daemon.py
+++ daemon.py
@@ -6,7 +6,7 @@ import database
 import bot
 
 DATABASE = database.Database()
-DATABASE.create_table('users', ['tgm_name', 'cf_login', 'chat_id'])
+DATABASE.create_table('users', ['tgm_name', 'cf_handle', 'chat_id'])
 
 bot.BOT.set_database(DATABASE)
 BOT_THREAD = threading.Thread(target=bot.BOT.run)
